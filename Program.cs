2584bfeef9ab7ad3712f0236695d04aced56cd22
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 13:13:40 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double salario, fgts;
+           
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+           Console.WriteLine("\n============= FGTS =============\n");
+           Console.ResetColor();
+
+           Console.WriteLine("Insira o valor total do sálario,\npara que seja calculada a parcela\ndo FGTS.\n");
+
+           Console.Write("Salário (R$)...: ");
+           salario = Convert.ToDouble(Console.ReadLine());
+
+           fgts = salario * 8 / 100;
+
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+           Console.WriteLine("\n=== Parcela do FGTS ===\n");
+           Console.ResetColor();
+
+           Console.WriteLine($"FGTS...........: {fgts:C}\n");
+        }
+    }
+}
